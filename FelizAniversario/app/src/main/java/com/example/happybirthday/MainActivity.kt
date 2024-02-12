637fcf7deb8a973ccb23d50e311eeded82e8e6c4
Jhonnathan-Jhonny <jhonnathan.rodrigues@aluno.uepb.edu.br>
2024-02-12 10:11:48 -0300

    aprendendo a colocar imagem no app

--- FelizAniversario/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ FelizAniversario/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(mensage = "Feliz aniversário Vitória!!!", from = "De Jhonnathan")
+                    GreetingImage(mensage = "Feliz aniversário Vitória!!!", from = "De Jhonnathan")
                 }
             }
         }
@@ -40,39 +44,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(mensage:String, from: String, modifier: Modifier = Modifier){
-    Surface(
-        color = Color.Blue,
-        //contentColor = Color.Red
-    ) {
+//    Surface(
+//        //color = Color.Blue,
+//        //contentColor = Color.Red
+//    ) {
         Column(
             verticalArrangement = Arrangement.Center,
-            modifier = modifier
-
+            modifier = modifier,
         ) {
             Text(
                 text = mensage,
                 fontSize = 60.sp,
                 fontFamily = FontFamily.Serif,
                 lineHeight = 116.sp,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                color = Color.Black
             )
             Text(
                 text = from,
                 fontSize = 36.sp,
                 modifier = Modifier
                     .padding(16.dp)
-                    .align(alignment = Alignment.End)
-
+                    .align(alignment = Alignment.End),
+                color = Color.Black
             )
         }
+   // }
+}
+
+@Composable
+fun GreetingImage(mensage: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+            mensage = mensage,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
 
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = false)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(mensage = "Feliz aniversário Vitória!!!", from = "De Jhonnathan")
+        GreetingImage(mensage = "Feliz aniversário Vitória!!!", from = "De Jhonnathan")
     }
 }
