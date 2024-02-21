d92d200a69bac7d7f760caaed9d880abb3f53359
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-02-21 08:18:33 -0300

    iniciando pratica artigo compose

--- ArtigoCompose/app/src/main/java/com/example/artigocompose/MainActivity.kt
+++ ArtigoCompose/app/src/main/java/com/example/artigocompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.artigocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.artigocompose.ui.theme.ArtigoComposeTheme
 
@@ -22,7 +25,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingImage(
+                        titulo = "Jetpack Compose tutorial",
+                        introducao = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                        texto = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
                 }
             }
         }
@@ -47,8 +53,10 @@ fun GreetingText(titulo: String, introducao: String, texto: String, modifier: Mo
 fun GreetingImage(titulo: String, introducao: String, texto: String, modifier: Modifier = Modifier){
     val image = painterResource(id = R.drawable.bg_compose_background)
     Image(
-        painter = image
+        painter = image,
+        contentDescription = "Imagem de início."
     )
+
     GreetingText(titulo = titulo, introducao = introducao, texto = texto)
 }
 
