62f314c8071fcb5cb7e3224e689581eba5784f6a
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-02-22 07:14:52 -0300

    prática de gerenciamento de tarefas finalizado

--- GerenciadorDeTarefas/app/src/main/java/com/example/gerenciadodetarefas/MainActivity.kt
+++ GerenciadorDeTarefas/app/src/main/java/com/example/gerenciadodetarefas/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.gerenciadodetarefas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gerenciadodetarefas.ui.theme.GerenciadoDeTarefasTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingImage()
                 }
             }
         }
@@ -30,17 +49,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = stringResource(R.string.all_tasks_completed),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(0.dp, 24.dp, 0.dp, 8.dp),
+            color = Color.Black
+        )
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(start = 38.dp),
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        GreetingText()
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GerenciadoDeTarefasTheme {
-        Greeting("Android")
+        GreetingImage()
     }
 }
