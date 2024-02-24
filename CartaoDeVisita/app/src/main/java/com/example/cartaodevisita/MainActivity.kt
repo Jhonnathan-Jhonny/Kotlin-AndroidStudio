c83a691807ea791332b4427a4f81485cb40f5642
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-02-23 22:27:15 -0300

    Finalizando app
    Organizações finais e add imagem de fundo

--- CartaoDeVisita/app/src/main/java/com/example/cartaodevisita/MainActivity.kt
+++ CartaoDeVisita/app/src/main/java/com/example/cartaodevisita/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.cartaodevisita
 
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,11 +17,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cartaodevisita.ui.theme.CartaoDeVisitaTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,48 +51,71 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     val imgTelefone = painterResource(id = R.drawable.telefone)
     val imgGit = painterResource(id = R.drawable.git)
     val imgEu = painterResource(id = R.drawable.profile_pic__2____copia)
-    Column (
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    val imgFundo = painterResource(id = R.drawable.fundo)
+    Box(
+        modifier = Modifier.fillMaxSize()
     ){
         Image(
-            painter = imgEu,
+            painter = imgFundo,
             contentDescription = null,
-            modifier = modifier.size(80.dp,80.dp)
+            modifier = Modifier.fillMaxSize()
         )
-        Text(
-            text = "Jhonnathan Rodrigues"
-        )
-        Text(
-            text = "Desenvolvedor android",
-            modifier.padding(bottom = 90.dp)
-        )
-        Contatos(imagem = imgTelefone,
-            descricao = "+55 (83) 9 9991-2744",
-        )
-        Contatos(imagem = imgGit, descricao = "Jhonnathan-Johnny")
-        Contatos(
-            imagem = imgInst,
-            descricao = "@Jhonnathan_Jhonny",
+        Column (
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(Alignment.Center)
+        ){
+            Image(
+                painter = imgEu,
+                contentDescription = null,
+                modifier = modifier.size(150.dp,150.dp),
             )
+            Text(
+                text = "Jhonnathan Rodrigues",
+                color = Color.Black,
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 30.sp
+            )
+            Text(
+                text = "Desenvolvedor android",
+                modifier.padding(bottom = 35.dp),
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
+            Contatos(
+                imagem = imgTelefone,
+                descricao = "+55 (83) 9 9991-2744",
+                Modifier.padding(top = 40.dp)
+            )
+            Contatos(imagem = imgGit, descricao = "Jhonnathan-Johnny", Modifier.padding(end = 5.dp))
+            Contatos(
+                imagem = imgInst,
+                descricao = "@Jhonnathan_Jhonny",
+            )
+        }
+
     }
 }
 
 @Composable
 fun Contatos(imagem: Painter, descricao: String, modifier: Modifier = Modifier){
     Row(
-        modifier.padding(10.dp),
+        modifier
+            .padding(10.dp)
     ){
         Image(
             painter = imagem,
             contentDescription = null,
             modifier = modifier
-                .size(50.dp,50.dp)
+                .size(50.dp, 50.dp)
                 .padding(end = 5.dp)
         )
         Text(
             text = descricao,
-            modifier.padding(top = 15.dp)
+            modifier.padding(top = 15.dp),
+            color = Color.Black,
+            fontSize = 20.sp
         )
     }
 
