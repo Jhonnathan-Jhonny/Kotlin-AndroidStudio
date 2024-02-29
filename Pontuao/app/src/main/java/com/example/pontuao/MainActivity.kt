0cc43e3aca4caf33bdf5416c5ca76a3792e743a9
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-02-29 17:08:21 -0300

    layout finalizado, para iniciar a interatividade

--- Pontuao/app/src/main/java/com/example/pontuao/MainActivity.kt
+++ Pontuao/app/src/main/java/com/example/pontuao/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.pontuao
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,41 +50,113 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LayoutInicial(modifier: Modifier = Modifier) {
-    Column (Modifier.fillMaxSize()){
+    Column (){
         Column (
-            modifier = Modifier.background(Color.Blue),
-            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .background(Color.Blue)
+                .weight(1f)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
             ){
-            Text(text = "0")
-            Row {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "+")
-                }
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "-")
-                }
-            }
-        }
-        Column (modifier = Modifier.background(Color.Red),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally){
-            Text(text = "0")
-            Row {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "+")
-                }
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "-")
-                }
-            }
-        }
-        Column (
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = "0",
+                fontSize = 150.sp,
+                color = Color.White
+            )
+            Text(
+                text = "Time 1",
+                Modifier.padding(bottom = 10.dp),
+                fontSize = 30.sp,
+                color = Color.White
+            )
+            Row(
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(end = 50.dp, bottom = 10.dp)
+                        .size(width = 150.dp, height = 75.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White)
 
+                    ) {
+                    Text(
+                        text = "-",
+                        fontSize = 40.sp,
+                        )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(bottom = 10.dp)
+                        .size(width = 150.dp, height = 75.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White)
+                ) {
+                    Text(
+                        text = "+",
+                        fontSize = 30.sp
+                    )
+                }
+            }
+        }
+        Column (modifier = Modifier
+            .background(Color.Red)
+            .weight(1f)
+            .fillMaxWidth(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        {
+            Text(
+                text = "0",
+                fontSize = 150.sp,
+                color = Color.White
+            )
+            Text(
+                text = "Time 2",
+                Modifier.padding(bottom = 10.dp),
+                fontSize = 30.sp,
+                color = Color.White
+                )
+            Row{
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(end = 50.dp, bottom = 10.dp)
+                        .size(width = 150.dp, height = 75.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White)
+                    ) {
+                    Text(
+                        text = "-",
+                        fontSize = 40.sp,
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(bottom = 10.dp)
+                        .size(width = 150.dp, height = 75.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White)
+                ) {
+                    Text(
+                        text = "+",
+                        fontSize = 30.sp
+                    )
+                }
+            }
+        }
+        Column (){
+            Button(
+                onClick = { /*TODO*/ },
+                modifier
+                    .fillMaxWidth()
+                    .background(Color.Yellow)
+                    .padding(top = 10.dp),
+                colors = ButtonDefaults.buttonColors(Color.Yellow),
+            ) {
+                Text(
+                    text = "Zerar",
+                    fontSize = 20.sp,
+                )
             }
         }
     }
