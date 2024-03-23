91bd6c12d3babdd5a3f50546f6ade8f872508ecf
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-03-23 16:18:21 -0300

    test

--- Pontuao/app/src/androidTest/java/com/example/pontuao/ExampleInstrumentedTest.kt
+++ Pontuao/app/src/androidTest/java/com/example/pontuao/ExampleInstrumentedTest.kt
@@ -36,11 +36,32 @@ class ExampleInstrumentedTest {
                 LayoutInicial()
             }
         }
-        // Clica no botão de adição uma vez
+
+        //testMaisMenos
+        // Clica no botão de adição duas vez
         composeTestRule.onNodeWithText(" +").performClick()
         composeTestRule.onNodeWithText(" +").performClick()
+        //subtraia
         composeTestRule.onNodeWithText(" -").performClick()
+        //verifique
         composeTestRule.onNodeWithText("1").assertExists()
+
+        //testZeroButton
+        // Clica no botão de subtração duas vezes
+        composeTestRule.onNodeWithText("+").performClick()
+        composeTestRule.onNodeWithText("+").performClick()
+
+        // Verifica se o placar foi dincrementado para 2
+        composeTestRule.onNodeWithText("2").assertExists()
+
+        // Clica no botão de zerar
+        composeTestRule.onNodeWithText("Zerar").performClick()
+
+        //Como existe dois zeros, ocorrera um erro, por isso a soma
+        composeTestRule.onNodeWithText("+").performClick()
+
+        // Verifica se o placar foi resetado para 0
+        composeTestRule.onNodeWithText("0").assertExists()
     }
 
 }
