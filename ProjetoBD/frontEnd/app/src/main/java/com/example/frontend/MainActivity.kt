5846c19a66443d11d55a309f539b6b767c8f5dd9
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-06-05 11:35:11 -0300

    Organizando layout das 3 telas

--- ProjetoBD/frontEnd/app/src/main/java/com/example/frontend/MainActivity.kt
+++ ProjetoBD/frontEnd/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,9 +37,17 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -94,116 +113,234 @@ fun InfoScreen(
         }
     }
 
-    if (usingEdit) {
-        Column {
-            OutlinedTextField(
-                value = tempName,
-                onValueChange = { tempName = it },
-                label = { Text(text = "Name") }
-            )
-            OutlinedTextField(
-                value = tempEmail,
-                onValueChange = { tempEmail = it },
-                label = { Text(text = "Email") }
-            )
-            Button(onClick = {
-                handleEdit(coroutineScope, context, tempName, tempEmail, namePrevious) { result ->
-                    result.onSuccess {
-                        // Update the user state and reset the temporary state
-                        user = user.copy(name = tempName, email = tempEmail)
-                        usingEdit = false
-                    }.onFailure {
-                        Toast.makeText(context, "Edition failed: ${it.message}", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }) {
-                Text(text = "Confirm")
-            }
-        }
-    } else {
+    Column(
+        modifier = Modifier
+            .background(colorResource(id = R.color.verdePrincipal))
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.registration_image),
+            contentDescription = "Image",
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "Name: ${user.name}",
+            fontSize = 20.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .padding(top = 30.dp)
+        )
+        Text(
+            text = "Email: ${user.email}",
+            fontSize = 20.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(start = 16.dp, top= 10.dp)
+        )
+        Text(
+            text = "Password: ${user.password}",
+            fontSize = 20.sp,
+            color = Color.Black,
+            modifier = Modifier.padding(start = 16.dp, bottom = 10.dp, top= 10.dp)
+        )
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start
+                .fillMaxWidth()
+                .padding(top = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Name: ${user.name}")
-            Text(text = "Email: ${user.email}")
-            Text(
-                text = "Password: ${user.password}",
-                modifier = Modifier.padding(bottom = 10.dp)
-            )
-
-            Row {
-                Button(onClick = {
+            Button(
+                onClick = {
                     handleDelete(coroutineScope, context, user.name, navController)
                     navController?.navigate("login") {
                         popUpTo("login") { inclusive = true }
                     }
-                }) {
-                    Text(text = "Delete")
-                }
+                },
+                Modifier
+                    .padding(bottom = 30.dp)
+                    .size(200.dp, 50.dp),
+                colors = ButtonDefaults.run { buttonColors(Color(R.color.verdeSecundário)) }
 
-                Button(onClick = { usingEdit = true }) {
-                    Text(text = "Edit")
-                }
+            ) {
+                Text(text = "Delete",
+                    fontSize = 20.sp
+                )
+            }
 
-                Button(onClick = {
+            Button(
+                onClick = { usingEdit = true },
+                Modifier
+                    .padding(bottom = 30.dp)
+                    .size(200.dp, 50.dp),
+                colors = ButtonDefaults.run { buttonColors(Color(R.color.verdeSecundário)) }
+            ) {
+                Text(text = "Edit",
+                fontSize = 20.sp)
+
+            }
+
+            Button(
+                onClick = {
                     navController?.navigate("login") {
                         popUpTo("login") { inclusive = true }
                     }
-                }) {
-                    Text(text = "Log Out")
-                }
+                },
+                Modifier
+                    .padding(bottom = 30.dp)
+                    .size(200.dp, 50.dp),
+                colors = ButtonDefaults.run { buttonColors(Color(R.color.verdeSecundário)) }
+            ) {
+                Text(text = "Log Out",
+                fontSize = 20.sp)
+
             }
         }
+
     }
 }
 
-
-
-
 @Composable
-fun LoginScreen(navController: NavController? = null){
+fun LoginScreen(navController: NavController? = null) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
+            .background(colorResource(id = R.color.verdePrincipal))
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        OutlinedTextField(value = email,
-            onValueChange = {email = it},
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth())
-        OutlinedTextField(value = password,
-            onValueChange = {password = it},
-            label = { Text("password") },
-            modifier = Modifier.fillMaxWidth())
-        Row {
-            Button(onClick = {
-                if (email.isNotBlank() && password.isNotBlank()) {
-                    handleLogin(coroutineScope,context,email,password,navController)
-                }
-            },
-                Modifier.padding(10.dp)) {
-                Text(text = "Login")
-            }
-            Button(onClick = { navController!!.navigate("registration") },
-                Modifier.padding(10.dp)) {
-                Text(text = "Cadastrar-se")
-            }
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp)
+                .align(Alignment.Start)
+        ) {
+            Text(
+                text = "Welcome",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.W400,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.imagemlogin),
+                contentDescription = "Image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .align(Alignment.End)
+            )
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_email_24),
+                        contentDescription = "Icon email"
+                    )
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Email
+                )
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_lock_outline_24),
+                        contentDescription = "Icon password"
+                    )
+                },
+                trailingIcon = {
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(
+                            painter = painterResource(
+                                id = if (passwordVisible) R.drawable.eye else R.drawable.eye_visible_off
+                            ),
+                            contentDescription = if (passwordVisible) "Hide password" else "Show password"
+                        )
+                    }
+                },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                )
+            )
         }
 
+        Divider(
+            color = Color.Black,
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .padding(10.dp, 0.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    if (email.isNotBlank() && password.isNotBlank()) {
+                        handleLogin(coroutineScope, context, email, password, navController)
+                    }
+                },
+                Modifier
+                    .padding(bottom = 30.dp)
+                    .size(200.dp, 50.dp),
+                colors = ButtonDefaults.run { buttonColors(Color(R.color.verdeSecundário)) }
+            ) {
+                Text(
+                    text = "Login",
+                    fontSize = 20.sp
+                )
+            }
+
+            Text(
+                text = "OR",
+                fontWeight = FontWeight.W500,
+                fontSize = 20.sp
+            )
+
+            Button(
+                onClick = { navController!!.navigate("registration") },
+                Modifier
+                    .padding(10.dp)
+                    .size(250.dp, 100.dp)
+                    .padding(20.dp, 0.dp, 20.dp, 20.dp),
+                colors = ButtonDefaults.run { buttonColors(Color.Transparent) },
+            ) {
+                Text(
+                    text = "Cadastrar-se",
+                    color = Color.Black,
+                    fontSize = 20.sp
+                )
+            }
+        }
     }
 }
+
+
 @Composable
 fun RegistrationScreen(navController: NavController? = null) {
     val context = LocalContext.current
@@ -212,39 +349,49 @@ fun RegistrationScreen(navController: NavController? = null) {
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    //1 -> null
-    // 2 -> true
-    // 3-> false
     var erro by remember { mutableIntStateOf(1) }
 
     Column(
         modifier = Modifier
+            .background(colorResource(id = R.color.verdePrincipal))
             .fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.registration_image),
+            contentDescription = "Image",
+            modifier = Modifier.size(200.dp)
+        )
+
         OutlinedTextField(
             value = name,
             onValueChange = { name = it },
             label = { Text("Name") },
             modifier = Modifier.fillMaxWidth()
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             value = email,
             onValueChange = { email = it },
             label = { Text("Email") },
             modifier = Modifier.fillMaxWidth()
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             value = password,
             onValueChange = { password = it },
             label = { Text("Password") },
             modifier = Modifier.fillMaxWidth()
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
         Button(
             onClick = {
                 if (name.isNotBlank() && email.isNotBlank() && password.isNotBlank()) {
@@ -254,12 +401,34 @@ fun RegistrationScreen(navController: NavController? = null) {
                     erro = 2
                 }
             },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Register")
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.run { buttonColors(Color(R.color.verdeSecundário)) },
+
+            ) {
+            Text("Register",
+                fontSize = 20.sp,
+            )
         }
+
         if (erro == 2) {
             Text("Nome de usuário já existe ou todos os campos devem ser preenchidos.")
+        }
+        Button(
+            onClick = {
+                navController?.navigate("login") {
+                    popUpTo("login") { inclusive = true }
+                }
+            },
+            Modifier
+                .padding(bottom = 30.dp,top=20.dp)
+                .size(110.dp, 50.dp)
+                .align(Alignment.Start),
+            colors = ButtonDefaults.run { buttonColors(Color.Transparent) }
+        ) {
+            Text(text = "Voltar",
+                fontSize = 20.sp
+            )
+
         }
     }
 }
@@ -376,8 +545,8 @@ private fun handleEdit(
 @Composable
 fun GreetingPreview() {
     FrontEndTheme {
-        LoginScreen()
+        //LoginScreen()
         //InfoScreen()
-        //RegistrationScreen()
+        RegistrationScreen()
     }
 }
