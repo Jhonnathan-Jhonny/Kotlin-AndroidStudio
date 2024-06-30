ab8857e6627ffbe4aefb239a137b882b3f02cfc0
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-06-30 15:44:28 -0300

    Update MainActivity.kt

--- MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,6 +17,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -32,7 +34,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MaterialDesignTheme {
-
+                WoofApp()
             }
         }
     }
@@ -62,13 +64,18 @@ fun WoofView(dataDog: DataDog, modifier: Modifier = Modifier){
                 modifier = Modifier
                     .size(64.dp)
             )
-            Column {
+            Column(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(),
+            ) {
                 Text(text = stringResource(id = dataDog.name))
-                Text(text = stringResource(id = dataDog.age )+" years old")
+                Text(text = "${dataDog.age} years old")
             }
         }
     }
 }
+
 
 @Composable
 fun WoofList(){
