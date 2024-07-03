2b46f1bdce4a2b2572ab712e5f2eba71abd5d78d
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-07-03 14:52:09 -0300

    Barra superior
    Scaffold Padding: No WoofApp, ajustei a chamada para Scaffold para passar paddingValues para o conteúdo principal. Este padding é utilizado no modificador do WoofList para garantir que o conteúdo abaixo da TopAppBar não seja sobreposto.
    
    WoofList Modifier: Adicionei um modifier opcional ao WoofList para que ele possa receber e aplicar o padding fornecido pelo Scaffold.

--- MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ MaterialDesign/app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.example.materialdesign
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -42,13 +36,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun WoofApp() {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        WoofList()
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) { paddingValues ->
+        // O conteúdo principal é ajustado para respeitar o padding fornecido pelo Scaffold
+        WoofList(modifier = Modifier.padding(paddingValues))
     }
 }
 
@@ -87,17 +84,40 @@ fun WoofView(dataDog: DataDog, modifier: Modifier = Modifier){
     }
 }
 
-
 @Composable
-fun WoofList(){
+fun WoofList(modifier: Modifier = Modifier){
     LazyColumn (
-        modifier = Modifier
+        modifier = modifier
             .padding(8.dp)
     ){
         items(listDog){ dog ->
             WoofView(dataDog = dog)
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(8.dp),
+                    painter = painterResource(R.drawable.ic_woof_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = "Woof",
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            }
+        },
+    )
 }
 
 @Preview
