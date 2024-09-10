06e0031d133a6a8f5831391cfef075f9dcdf391e
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2024-09-10 15:16:45 -0300

    retornando ao estudo
    iniciando prática com objetivo de entender o viewModel

---
+++ ViewModelPraticaJogoformeapalavra/app/src/main/java/com/example/viewmodelpratica_jogoformeapalavra/helpers/GameUiState.kt
@@ -0,0 +1,9 @@
+package com.example.viewmodelpratica_jogoformeapalavra.helpers
+
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val currentWordCount: Int = 1,
+    val score: Int = 0,
+    val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false
+)
