0c73103049e60e1395d3d0737ab4e34bda265ed5
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2026-06-09 13:42:12 -0300

    Create RoomDataBase.kt

---
+++ inventory-app-starter/app/src/main/java/com/example/inventory/data/RoomDataBase.kt
@@ -0,0 +1,30 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+    companion object {
+        @Volatile // leituras e gravações são de e para a memória principal.
+        //garantir que o valor de Instance esteja sempre
+        //atualizado e seja o mesmo para todas as linhas de execução.
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            //synchronized significa que somente uma linha de execução poderá entrar nesse bloco de código por vez
+            // Busca evitar conflitos/disputa
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
