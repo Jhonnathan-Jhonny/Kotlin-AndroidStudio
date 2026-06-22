0dc1fa9de09b82b72529711de7ceaf7b314c9324
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2026-06-22 15:44:20 -0300

    feat: classe do banco de dados implementado

--- PraticaRoom-bus/app/src/main/java/com/example/busschedule/data/BusDataBase.kt
+++ PraticaRoom-bus/app/src/main/java/com/example/busschedule/data/BusDataBase.kt
@@ -1,8 +1,33 @@
 package com.example.busschedule.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 
 @Database(entities = [BusSchedule::class], version = 1, exportSchema = false)
-abstract class BusDataBase {
+abstract class BusDataBase : RoomDatabase() {
+
+    abstract fun busDao(): BusDao
+
+    //O companion object serve para criar membros (variáveis e funções)
+    // que pertencem à própria classe, e não a instâncias específicas dela.
+    companion object {
+        @Volatile
+        private var INSTANCE: BusDataBase? = null
+
+        fun getDatabase(context: Context): BusDataBase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    klass = BusDataBase::class.java,
+                    name = "bus_database"
+                )
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+
+    }
 }
