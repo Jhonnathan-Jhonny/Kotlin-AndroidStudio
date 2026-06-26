c977d838240e7f9af187e9fe4632019ed2d5683b
Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
2026-06-26 09:49:31 -0300

    feat: implementando repositório de preferencia do usuário.

--- dessert-release-DataStore/app/build.gradle.kts
+++ dessert-release-DataStore/app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.remote:remote-creation-compose:1.0.0-alpha13")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
---
+++ dessert-release-DataStore/app/src/main/java/com/example/dessertrelease/data/UserPreferencesRepository.kt
@@ -0,0 +1,40 @@
+package com.example.dessertrelease.data
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class UserPreferencesRepository(
+    private val dataStore: DataStore<Preferences>,
+){
+    private companion object {
+        val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+        const val TAG = "UserPreferencesRepo"
+    }
+
+    suspend fun saveLayoutPreference(isLinearLayoutManager: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
+
+    val isLinearLayoutManager: Flow<Boolean> = dataStore.data
+        .catch {
+            if(it is IOException){
+                Log.e(TAG,"Error reading preferences",it)
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map {
+            it[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
+}
